977c4e4bd8514b879e25c7166f414225f25acded
enmebaraggesi <111730049+enmebaraggesi@users.noreply.github.com>
2023-03-04 12:32:32 +0100

    Create Application.java

---
+++ Application.java
@@ -0,0 +1,37 @@
+public class Application {
+    public static void main(String[] args) {
+        Engine petrol14 = new R4_16V_14();
+        Engine petrol20 = new R4_16V_20();
+        Engine diesel16 = new R4_16V_16HDI();
+        Engine diesel20 = new R4_FAP_20HDI();
+
+        Car peugeot307_14 = new Car(petrol14);
+        peugeot307_14.gainSpeed(100, 10);
+        peugeot307_14.gainSpeed(100, 10);
+        peugeot307_14.gainSpeed(100, 10);
+
+        System.out.println();
+
+        Car peugeot307_20 = new Car(petrol20);
+        peugeot307_20.gainSpeed(25, 5);
+        peugeot307_20.gainSpeed(50, 5);
+        peugeot307_20.gainSpeed(75, 5);
+        peugeot307_20.gainSpeed(100, 5);
+        peugeot307_20.brake(70);
+
+        System.out.println();
+
+        Car peugeot307_16diesel = new Car(diesel16);
+        peugeot307_16diesel.gainSpeed(70, 20);
+        peugeot307_16diesel.brake(100);
+
+        System.out.println();
+
+        Car peugeot307_20diesel = new Car(diesel20);
+        peugeot307_20diesel.gainSpeed(100, 60);
+        peugeot307_20diesel.brake(50);
+        peugeot307_20diesel.gainSpeed(75, 3);
+        peugeot307_20diesel.gainSpeed(50, 5);
+
+    }
+}
