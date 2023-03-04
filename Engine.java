7ce5aa8d6c0af1a4b54a522d9e0a498999a58523
enmebaraggesi <111730049+enmebaraggesi@users.noreply.github.com>
2023-03-04 13:22:23 +0100

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -1,5 +1,5 @@
 public interface Engine {
     double speedUp(double percent, double seconds);
-    public void setActualRPM(double actualRPM);
+    void setActualRPM(double actualRPM);
     double getActualRPM();
 }
